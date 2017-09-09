e4d6871091f6e19b9fcbbc44fd74b300eba73bab
wang <wanggs@getui.com>
2017-09-09 18:24:06 +0800

    add toolbar

--- app/src/main/java/com/xiuxiuing/hotlook/MainActivity.java
+++ app/src/main/java/com/xiuxiuing/hotlook/MainActivity.java
@@ -1,17 +1,29 @@
 package com.xiuxiuing.hotlook;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
     }
 
     @Override
@@ -20,19 +32,23 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.start:
-                Toast.makeText(this, "开始游戏", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.over:
-                Toast.makeText(this, "结束游戏", Toast.LENGTH_SHORT).show();
-                break;
+    private Toolbar.OnMenuItemClickListener onMenuItemClickListener = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.start:
+                    Toast.makeText(MainActivity.this, "开始游戏", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.over:
+                    Toast.makeText(MainActivity.this, "结束游戏", Toast.LENGTH_SHORT).show();
+                    break;
 
-            default:
-                break;
+                default:
+                    break;
+            }
+            return true;
         }
-        return super.onOptionsItemSelected(item);
-    }
+    };
+
+
 }
