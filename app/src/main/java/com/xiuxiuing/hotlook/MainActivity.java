1e816f4fd7f66d26505e15f4a99fe4d94ffcd855
wang <wanggs@getui.com>
2017-09-13 21:04:13 +0800

    add drawerlayout

--- app/src/main/java/com/xiuxiuing/hotlook/MainActivity.java
+++ app/src/main/java/com/xiuxiuing/hotlook/MainActivity.java
@@ -1,6 +1,11 @@
 package com.xiuxiuing.hotlook;
 
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,10 +16,16 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.nav_view)
+    NavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +34,40 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
-        toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navView.setNavigationItemSelectedListener(this);
+
+//        toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
 
     private Toolbar.OnMenuItemClickListener onMenuItemClickListener = new Toolbar.OnMenuItemClickListener() {
         @Override
         public boolean onMenuItemClick(MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.start:
+                case R.id.nav_camera:
                     Toast.makeText(MainActivity.this, "开始游戏", Toast.LENGTH_SHORT).show();
                     break;
-                case R.id.over:
+                case R.id.nav_gallery:
                     Toast.makeText(MainActivity.this, "结束游戏", Toast.LENGTH_SHORT).show();
                     break;
 
@@ -51,4 +79,20 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_camera:
+                Toast.makeText(MainActivity.this, "开始游戏", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_gallery:
+                Toast.makeText(MainActivity.this, "结束游戏", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
